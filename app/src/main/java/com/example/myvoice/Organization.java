4dc5be9266ba2b1716f770cd86628e91166c7e76
marvo99 <marvino547@gmail.com>
2021-02-16 14:36:01 +0300

    Final Changes

--- app/src/main/java/com/example/myvoice/Organization.java
+++ app/src/main/java/com/example/myvoice/Organization.java
@@ -1,6 +1,6 @@
 package com.example.myvoice;
 
-//Quick email connection without having to go through all the code
+//Quick email connection without having to go through to look for the section
 public class Organization {
     public static final String EMAIL ="voiceproject38@gmail.com";
     public static final String PASSWORD ="Gismima120";
